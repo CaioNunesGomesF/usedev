a1fa99ed7c1451438ed35940f928cfd19f4decf0
Caio Nunes <caiongomes@gmail.com>
2025-09-05 09:33:19 -0300

    Finalizado

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Categoria {
+  final String nome;
+  final String imagePath;
+
+  const Categoria({required this.nome, required this.imagePath});
+}
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -7,40 +14,168 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+final List<Categoria> categorias = const [
+  Categoria(nome: 'Roupas', imagePath: 'assets/Roupas.png'),
+  Categoria(nome: 'Decoração', imagePath: 'assets/Decoracao.png'),
+  Categoria(nome: 'Canecas', imagePath: 'assets/Canecas.png'),
+  Categoria(nome: 'Acessórios', imagePath: 'assets/Acessorios.png'),
+];
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 28, 3, 91),
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 229, 229, 229),
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
         title: Image.asset('assets/logo.png', height: 40),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.person_2_outlined)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.person_outline)),
           IconButton(
-            icon: Icon(Icons.shopping_cart_outlined),
             onPressed: () {},
+            icon: Icon(Icons.shopping_cart_outlined),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            hintText: 'Pesquisar...',
-            suffixIcon: Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Icon(Icons.search),
+      body: ListView(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: 'O que você procura?',
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: Icon(Icons.search),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
             ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-              borderSide: BorderSide.none,
-            ),
-            filled: true,
-            fillColor: Colors.white,
           ),
-        ),
+          SizedBox(height: 20),
+          Image.asset('assets/hero.png', height: 500),
+          SizedBox(height: 40),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset(
+                'assets/detalhe.png',
+                width: double.infinity,
+                height: 400,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Hora de abraçar seu',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 85, 224),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      ' lado Geek!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 124, 255, 2),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.2,
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 121, 11, 247),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: Text(
+                          'Ver as novidades!',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 50),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              children: [
+                Text(
+                  'Categoria',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 1, 27, 100),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'De roupas a gadgets tecnológicos temos tudo para atender suas paixões e hobbies com estilo!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 1, 27, 100),
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ListView.builder(
+                  itemCount: categorias.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    final categoria = categorias[index];
+                    return Column(
+                      children: [
+                        Image.asset(categoria.imagePath, height: 300),
+                        const SizedBox(height: 6),
+                        Text(
+                          categoria.nome,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 1, 27, 100),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
